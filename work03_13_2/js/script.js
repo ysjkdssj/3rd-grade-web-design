d29c730d6b077fb1d5de3d6198c9b338a48491d2
유성준 <93107071+ysjkdssj@users.noreply.github.com>
2023-03-13 12:50:50 +0900

    a-1 최종 완성본

--- work03_13_2/js/script.js
+++ work03_13_2/js/script.js
@@ -13,14 +13,14 @@ var timer = setInterval("autoslide()",3000);
 
 function autoslide() {
     $(slide[sno]).animate({
-        left: "-100%"
+        top: "100%"
     },1000,function() {
-        $(this).css({left:"100%"});
+        $(this).css({top:"-100%"});
     });
     sno++;
     if(sno > eno) sno = 0;
     $(slide[sno]).animate({
-        left:"0"
+        top:"0"
     },1000);
 }
 
